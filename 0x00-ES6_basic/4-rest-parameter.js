7b87fc4c3e363c72a04a2dc66a50189151452589
JumaOchi <ochiengjuma95@gmail.com>
2023-01-25 21:02:02 +0000

    rest parameter sytax

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,11 @@
+export default function returnHowManyArguments(...args) { // eslint-disable-line no-unused-vars
+  return arguments.length;
+}
+
+// export default function returnHowManyArguments(...args) {
+//   let count = 0;
+//   for (const i of args) { // eslint-disable-line no-unused-vars
+//     count += 1;
+//   }
+//   return count;
+// }
