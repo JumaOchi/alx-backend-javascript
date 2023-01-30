a5176a259e1eb6fdd24cf988eee358008181148d
JumaOchi <ochiengjuma95@gmail.com>
2023-01-30 14:04:41 +0000

    building task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,22 @@
-/* Impplemmentinng a class Building
+/*Impplemmentinng a class Building
 */
 export default class Building {
-  constructor(sqft) {
-    this.sqft = sqft;
+    constructor(sqft) {
+      this.sqft = sqft;
+      if (this.constructor !== Building) {
+        if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error(
+            'Class extending Building must override evacuationWarningMessage',
+          );
+        }
+      }
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    set sqft(value) {
+      this._sqft = value;
+    }
   }
-}
