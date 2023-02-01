a35c49317c19b13f964cd4085ba38b83c3352878
JumaOchi <ochiengjuma95@gmail.com>
2023-02-01 18:26:31 +0000

    using map to obtain all id's

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
 // Create a function getListStudentIds that returns an array of ids from a list of object.
 
-export default function getListStudentsIds(arr) {
-  if (arr.constructor === Array) {
-    return arr.map((arr) => arr.id);
+export default function getListStudentsIds(students) {
+  if (students.constructor === Array) {
+    return students.map((student) => student.id);
   }
   return [];
 }
