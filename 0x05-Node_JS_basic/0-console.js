678227aa7c6eac230c0691c6b47e23961e47625d
JumaOchi <ochiengjuma95@gmail.com>
2023-03-24 14:43:49 +0000

    Initial commit node

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display a message on the STDOUT
+ * @param {string} msg The message to display
+ *
+ */
+const displayMessage = (msg) => {
+	console.log(msg);
+};
+
+module.exports = displayMessage;
